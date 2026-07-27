9c152bb80059e44ffd9647bd395bb06309eefbed
Raven <Greyvira777@gmail.com>
2026-07-27 06:06:40 -0400

    Update KHFM_EnemyConfig.lua

--- files/KHFM_EnemyConfig.lua
+++ files/KHFM_EnemyConfig.lua
@@ -667,7 +667,7 @@ local INTERNAL_CONFIG = {
         },
         ["Darkside"] = {
             source_bgm = "music145.win32.scd",
-            bgm_slot = 0, music_priority = 100,
+            bgm_slot = 1, music_priority = 100,
             model_codes = { "xa_di_3000", "xa_di_3001", "xa_di_3009" },
             fingerprints = {},
             context_bindings = {
