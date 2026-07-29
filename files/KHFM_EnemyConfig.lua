1b62240f4ff1d3dcc80529a339ab53319f183ab6
Raven <Greyvira777@gmail.com>
2026-07-29 15:05:35 -0400

    Update KHFM_EnemyConfig.lua

--- files/KHFM_EnemyConfig.lua
+++ files/KHFM_EnemyConfig.lua
@@ -248,7 +248,7 @@ local ENEMY_SETTINGS = {
     ["Ice Titan"] = { 
         MAX_HP = 4000, DAMAGE_TAKEN = 1.00, DAMAGE_FLOOR = nil, DAMAGE_CEILING = nil, ANIMATION_SPEED = {}, OVERALL_SPEED = nil, BATTLE_THEME = nil },
     ["Sephiroth"] = { 
-        MAX_HP = 2100, DAMAGE_TAKEN = 1.00, DAMAGE_FLOOR = 25, DAMAGE_CEILING = nil, 
+        MAX_HP = 3000, DAMAGE_TAKEN = 1.00, DAMAGE_FLOOR = 25, DAMAGE_CEILING = nil, 
         ANIMATION_SPEED = {}, 
         OVERALL_SPEED = nil, 
         BATTLE_THEME = "KHFM_SephirothTheme.win32.scd",
